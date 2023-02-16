105d4d4a7f68d542accfe19e51fbf8ad4b36127a
CasianLW <ciorbcasian@gmail.com>
2023-02-16 12:20:55 +0100

    dev du controller qui va nous faire fonctionner le CRUD pour les locaitons

--- BACKEND-TouristApp/app/Http/Controllers/LocationController.php
+++ BACKEND-TouristApp/app/Http/Controllers/LocationController.php
@@ -3,36 +3,69 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Location;
 
 class LocationController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+       $locations = Location::all();
+       return response()->json($locations);
     }
+
 
     public function store(Request $request)
     {
-        //
+        $this->validate($request,[
+            'name'=> 'required',
+            'slug'=> 'required',
+            'lat' => 'required',
+            'lng' => 'required'
+        ]);
+        $location = new Location();
+
+        $location->name =$request->input('name');
+        $location->slug =$request->input('slug');
+        $location->lat =$request->input('lat');
+        $location->lng =$request->input('lng');
+
+        $location->save();
+        
+        return response()->json(($location));
     }
+
+
     public function show($id)
     {
-        //
+        $location = Location::find($id);
+       return response()->json($location);
     }
 
 
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request,[
+            'name'=> 'required',
+            'slug'=> 'required',
+            'lat' => 'required',
+            'lng' => 'required'
+        ]);
+        $location = Location::find($id);
+
+        $location->name =$request->input('name');
+        $location->slug =$request->input('slug');
+        $location->lat =$request->input('lat');
+        $location->lng =$request->input('lng');
+
+        $location->save();
+        
+        return response()->json(($location));
     }
 
     public function destroy($id)
     {
-        //
+        $location = Location::find($id);
+        $location->delete();
+       return response()->json("Location #$id removed !");
     }
 }
