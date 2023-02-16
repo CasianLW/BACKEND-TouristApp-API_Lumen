2d881f1301487afd6b332268b86c2733e8b0b6b9
CasianLW <ciorbcasian@gmail.com>
2023-02-16 12:20:25 +0100

    Creation des routed de l'application

--- BACKEND-TouristApp/routes/web.php
+++ BACKEND-TouristApp/routes/web.php
@@ -12,6 +12,21 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+// routes pour les locations
+$router->get('/api/locations','LocationController@index');
+$router->get('/api/locations/{id}','LocationController@show');
+$router->post('/api/locations/','LocationController@store');
+$router->put('/api/locations/update/{id}','LocationController@update');
+
+$router->delete('/api/locations/{id}','LocationController@destroy');
+
+// places
+$router->get('/api/locations/{id}/places','PlaceController@show');
+$router->post('/api/locations/{id}/places','PlaceController@store');
+
+$router->put('/api/places/{id}','PlaceController@update');
+$router->delete('/api/places/{id}','PlaceController@destroy');
+
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
