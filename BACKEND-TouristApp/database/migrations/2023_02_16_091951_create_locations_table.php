9d458592197b1ea06bd4792a8bdc2b9019b72f8b
CasianLW <ciorbcasian@gmail.com>
2023-02-16 10:27:35 +0100

    migrations setup

---
+++ BACKEND-TouristApp/database/migrations/2023_02_16_091951_create_locations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps('created_at');
+            $table->timestamps('updated_at');
+            $table->string('name');
+            $table->string('slug');
+            $table->float('lat');
+            $table->float('lng');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+};
