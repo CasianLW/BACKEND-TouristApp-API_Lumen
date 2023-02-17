7096b6730ddcf75e2502d7bc74e2c63af93b5bb9
CasianLW <ciorbcasian@gmail.com>
2023-02-17 12:08:27 +0100

    gestion erreurs et resolution bugs

--- BACKEND-TouristApp/app/Http/Controllers/LocationController.php
+++ BACKEND-TouristApp/app/Http/Controllers/LocationController.php
@@ -4,13 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Location;
+use Illuminate\Support\Str;
+
 
 class LocationController extends Controller
 {
     public function index()
     {
+        try {
+
        $locations = Location::all();
-       return response()->json($locations);
+    //    return response()->json($locations);
+       return response()->json($locations, 200);
+
+    } catch (\Throwable $th) {
+        return response()->json(['error' => "Error: ".$th], 404);
+    }
     }
 
 
@@ -18,39 +27,72 @@ class LocationController extends Controller
     {
         $this->validate($request,[
             'name'=> 'required',
-            'slug'=> 'required',
-            'lat' => 'required',
-            'lng' => 'required'
+            'lat' => 'required|numeric',
+            'lng' => 'required|numeric'
         ]);
+        try {
+
         $location = new Location();
 
         $location->name =$request->input('name');
-        $location->slug =$request->input('slug');
+
+        $location->slug =Str::slug($location->name, '-') ;
+        $slug = Str::slug($location->name, '-');
+        // Check if the slug is unique
+        if (Location::where('slug', $slug)->exists()) {
+            $i = 1;
+
+            // Append incremental numbers to the slug until a unique one is found
+            while (Location::where('slug', $slug . '-' . $i)->exists()) {
+                $i++;
+           }
+            $slug .= '-' . $i;
+        }
+
+        $location->slug = $slug;
         $location->lat =$request->input('lat');
         $location->lng =$request->input('lng');
 
         $location->save();
         
-        return response()->json(($location));
+        // return response()->json(($location));
+       return response()->json($location, 201);
+    } catch (\Throwable $th) {
+        return response()->json(['error' => "Error: ".$th], 404);
+    }
+
     }
 
 
     public function show($id)
     {
-        $location = Location::find($id);
-       return response()->json($location);
+        try {
+        
+        $location = Location::findOrFail($id);
+    //    return response()->json($location);
+       return response()->json($location, 200);
+
+    }catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+        return response()->json(['error' => "Error: Place not found"], 404);
+    } catch (\Throwable $th) {
+        return response()->json(['error' => "Not Found ! ".$th], 404);
+    }
     }
 
 
     public function update(Request $request, $id)
     {
+
         $this->validate($request,[
             'name'=> 'required',
             'slug'=> 'required',
-            'lat' => 'required',
-            'lng' => 'required'
+            'lat' => 'required|numeric',
+            'lng' => 'required|numeric'
         ]);
-        $location = Location::find($id);
+        try {
+            //code...
+        
+        $location = Location::findOrFail($id);
 
         $location->name =$request->input('name');
         $location->slug =$request->input('slug');
@@ -59,13 +101,28 @@ class LocationController extends Controller
 
         $location->save();
         
-        return response()->json(($location));
+        // return response()->json(($location));
+        return response()->json($location, 200);
+
+    }catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+        return response()->json(['error' => "Error: Place not found"], 404);
+    } catch (\Throwable $th) {
+        return response()->json(['error' => "Error: Unprocessable Entity  ".$th], 422);
+    }
     }
 
     public function destroy($id)
     {
-        $location = Location::find($id);
+        try {
+        $location = Location::findOrFail($id);
         $location->delete();
-       return response()->json("Location #$id removed !");
+    //    return response()->json("Location #$id removed !");
+       return response()->json(['message' => "Location #$id removed !"], 204);
+
+    }catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+        return response()->json(['error' => "Error: Place not found"], 404);
+    } catch (\Throwable $th) {
+        return response()->json(['error' => "Error: Not Found ".$th], 404);
+    }
     }
 }
